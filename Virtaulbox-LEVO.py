2d106dfa91eb991b3d7d102b5e1bed54055e897b
LEVO <90807226+HydrLEVO@users.noreply.github.com>
2021-09-19 10:05:14 +1000

    Add files via upload

---
+++ Virtaulbox-LEVO.py
@@ -0,0 +1,564 @@
+import time
+import os
+import platform
+
+
+print("                                                LEVO_VirtaulBox")
+print("""
+                                       ________________________________________________________
+                                       |                                                      |
+                                       |  [1] select-operation                                |
+                                       |                                                      |
+                                       |  [2] data-storage_box                                |
+                                       |  [3] info                                            | 
+                                       |                                                      |      
+                                       |                                                      |
+                                       |______________________________________________________|
+""")
+
+if select == '1':
+   print("""
+   [1] LEVO
+   [2] LEVO_DOS
+   [3] LEVO_Lepard
+   [4] CYBER_C DOS
+   
+   """)
+   
+if select == '1':
+    print("""
+                                                                  LEVO_GRUB
+                                            ___________________________________________________________
+                                            |  [1] LEVO_BOOTUP​​​​​                                         |
+                                            |  [2] LEVO_RECOVERING_SYSTEM                              |
+                                            |  [4] DATA_STORAGE                                        |
+                                            |                                                          | 
+                                            |________________________LEVO_BETA_________________________|
+""")
+select = input(">>> ")
+if select == '4':
+    print("""
+    storage-data = 34.99_.9,328 [GIG'S]
+    
+    ████████████████████████████████████████████████████████████████████████████████████████████████████
+    installation satisfied = compleated
+    
+    installation = compleate
+""")
+select = input(">> ") 
+
+
+if select == '1':
+ print("""
+                                                        LEVO_LOADUP
+""")
+print("""
+                                                 loading up resource's
+                                                
+                                           do not interfere with the loadup system
+                                           
+                                                                   
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    BBBBBBBBBBBBBBBBBBBB
+                                                    BBBBBBBBBBBBBBBBBBBB         
+                                                    
+                                               TYPE 'LOADUP' TO ENTER YOUR DESKTOP                   
+""")
+
+select = input("[?]: ")
+
+if select == 'loadup':
+    print("""
+                                                              LEVO
+""")
+print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] free Text
+                                                        [3] To Open File Exploer
+                                                        [4] storage_system
+                                                        [5] To Open Terminal
+                                                        
+                                                        
+                                                        
+                                                        
+                                                        [q] To Close OS Safley
+""")
+
+select = input(">: ")
+if select == 'q':
+    os.system('exit')
+    
+
+if select == '1':
+    print("""
+    [1] LEVO_ALPHA
+    [2] LEVO_BETA2.1
+    [3] LEVO_LEPARD
+    [4] LEVO_dos
+    """)
+select = input(">: ")
+if select == '1':
+        print("""
+                                                                  LEVO_ALPHA-GRUB
+                                            ___________________________________________________________
+                                            |  [1] LEVO_BOOTUP​​​​​                                         |
+                                            |  [2] LEVO_RECOVERING_SYSTEM                              |
+                                            |  [4] DATA_STORAGE                                        |
+                                            |                                                          | 
+                                            |________________________LEVO_BETA_________________________|
+    """)
+select = input(">>> ")
+
+if select == '2':
+    print("""
+                                                               LEVO_BETA2.1-GRUB
+                                            ___________________________________________________________
+                                            |  [1] LEVO_BOOTUP​​​​​                                         |
+                                            |  [2] LEVO_RECOVERING_SYSTEM                              |
+                                            |  [4] DATA_STORAGE                                        |
+                                            |                                                          | 
+                                            |________________________LEVO_BETA_________________________|
+""")
+    
+if select == '1':
+    print("""
+
+please wait for installation to compleate...
+
+installing LEVO_ALPHA
+
+████████████████████████████████████████████████████████████████████████████████████████████████████
+
+installation = LEVO_ALPHA.os
+installation = LEVO_PACH.sys
+installation = satisfied
+
+install package = Compleated
+
+type in 'LU' to enter loadup
+""")
+select = input(">> ")
+if select == 'lu':
+      print("""
+                                                        LEVO_LOADUP
+      """)
+print("""
+                                                 loading up resource's
+                                                
+                                           do not interfere with the loadup system
+                                           
+                                                                   
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    BBBBBBBBBBBBBBBBBBBB
+                                                    BBBBBBBBBBBBBBBBBBBB         
+                                                    
+                                               TYPE 'LOADUP' TO ENTER YOUR DESKTOP                   
+""")
+
+select = input("[?]: ")
+
+if select == 'loadup':
+        print("please press 's' to start LEVO_BETA2.1")
+        select = input (">: ")
+        
+if select == 'f':
+    print("""
+                                                                  LEVO_ALPHA-GRUB
+                                            ___________________________________________________________
+                                            |  [1] LEVO_BOOTUP​​​​​                                         |
+                                            |  [2] LEVO_RECOVERING_SYSTEM                              |
+                                            |  [4] DATA_STORAGE                                        |
+                                            |                                                          | 
+                                            |________________________LEVO_BETA_________________________|
+    """)
+    select = input(">>> ")
+    
+    if select == '1':
+      print("""
+                                                        LEVO_LOADUP
+      """)
+print("""
+                                                 loading up resource's
+                                                
+                                           do not interfere with the loadup system
+                                           
+                                                                   
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    UUU
+                                                    BBBBBBBBBBBBBBBBBBBB
+                                                    BBBBBBBBBBBBBBBBBBBB         
+                                                    
+                                               TYPE 'LOADUP' TO ENTER YOUR DESKTOP                   
+""")
+
+select = input("[?]: ")
+
+if select == 'loadup':
+    print("""
+                                                          LEVO_ALPHA
+    """)
+    print("""
+                                                     [1] LEVO_virtaul-box
+                                                     [2] LEVO_user-account
+                                                     [3] LEVO_storage-system
+                                                     
+    
+                                                     [q] QUIT
+""")
+select = input(">: ")
+if select == '2':
+    print("""
+                                                    user-account  ALPHA
+                                                    
+                                                 [1] user station
+                                                 [2] info
+                   
+    """)
+select = input(">> ")
+if select == '1':
+        print("""
+                                               user is you
+                                      this is a user station by LEVO ALPHA 
+                                      
+                                            this is coming soon.
+        """) 
+        
+if select == '2':
+        print("""
+                                                   this user info is for no reason
+                                                   
+                                                      this is for a declear.
+        """)
+
+if select == '1':
+    print("""
+    [1] LEVO_ALPHA
+    [2] LEVO_BETA2.1
+    [3] LEVO_LEPARD
+    [4] LEVO_dos
+    """)
+select = input(">: ")
+    
+
+if select == '1':
+	    
+	print("please press 'S' to start LEVO_ALPHA.os")
+	select = input(">: ")
+	
+if select == 's':
+    print("""
+                                                             LEVO_ALPHA
+           """)
+print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] To Open Text Editor
+                                                        [3] storage_system
+                                                        [4] To Open Terminal
+                                                        
+                                                        [7] hydra_echo
+                                                       
+                                                       
+                                                       
+                                                       [q] To Close OS Safley
+           """)
+           
+select = input(">: ")
+
+if select == '2':
+    write = input(str(""))
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system('exit')
+
+if select == '2':
+    write = input(str(""))
+
+if select == '2':
+	freeWrite = input(str(""))
+
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system('exit')
+
+
+
+
+if select == '1':
+	select = input("[?]: ")
+	print("please press 'S' to start LEVO_ALPHA.os")
+	select = input("[?]: ")
+	
+	if select == 's':
+	
+           print("""
+                                                             LEVO_ALPHA
+           """)
+           print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] To Open Text Editor
+                                                        [3] storage_system
+                                                        [4] To Open Terminal
+                                                        
+                                                        [7] hydra_echo
+                                                       
+                                                       
+                                                       
+                                                       [q] To Close OS Safley
+""")
+select = input(">: ")
+          
+if select == '2':
+    write = input(str(""))
+
+if select == '2':
+	freeWrite = input(str(""))
+
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system("exit")
+
+
+    write = input(str(""))
+	  
+
+         
+	 
+
+if select == '3':
+   print("""
+                 LEVO_STORAGE-SYSTEM
+      
+                 (C/_[USER_])>
+                 Storage-System == 34.00_GIGS
+                 package = C/_
+                 user_account = LEVO.os
+    
+                 INSTALLATION_LEVO.os
+                 installation>>
+    
+               _________________________________________________________________________________________________________
+               █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+                 installation_LEVO.os = satisfied
+                 lev-installation = 100%
+    
+                 installation == Compleated
+""")
+    
+if select == '4':
+    print("""
+                    Terminal_lev-Hydra
+    
+                    [C/_(USER)]>>
+                    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+""")
+
+
+if select == 'q':
+	             os.system('exit')
+
+if select == '2':
+	os.startfile('cyber.py')
+	os.startfile('edit.pyw')
+
+if select == '3':
+    print("""
+    FILES CANNOT BE SHOWED DUE TO (BETA-RELEASE)
+""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+	os.system('exit')
+    
+if select == '2':
+    print("""
+    [1] LEVO_Core16.REC
+    [2] LEVO_Core17.REC
+    [3] LEVO_Recovery-System.REC
+    [4] back
+""")
+select = input(">: ")
+if select == '4':
+    os.system('exit')
+   
+if select == '2':
+    print("""
+    strage data = 505,385 GIG'S
+    """)
+   
+if select == '3':
+    print("""
+    this is a virtaul box powerd by LEVO-Corporation to access every LEVO OS
+    """)
